1b2a06bf33d1f3c73f5ec3ea63ceb988aaa41036
RazvanPorojan <47414261+RazvanPorojan@users.noreply.github.com>
2019-04-16 13:57:05 +0200

    changed grid size to 5

--- js/application.js
+++ js/application.js
@@ -1,6 +1,6 @@
 // Wait till the browser is ready to render the game (avoids glitches)
 window.requestAnimationFrame(function () {
-  game_manager = new GameManager(4, KeyboardInputManager, HTMLActuator, LocalScoreManager);
+  game_manager = new GameManager(5, KeyboardInputManager, HTMLActuator, LocalScoreManager);
 });
 
 function handle_undo() {
